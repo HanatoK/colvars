7ebfae35c7927085c0544015543d43f0d02333ad
hanatok <summersnow9403@gmail.com>
2018-12-03 10:43:32 +0800

    Fix compiler errors.

--- src/colvarbias_meta.cpp
+++ src/colvarbias_meta.cpp
@@ -1688,12 +1688,12 @@ void colvarbias_meta::write_pmf()
     time_independent_grid->reset();
     pmf->add_grid(*hills_energy);
     cvm::real kbt = cvm::temperature() * cvm::boltzmann();
-    time_independent_grid->add_grid(*hills_energy, kbt)
+    time_independent_grid->add_grid(*hills_energy, kbt);
     if (!well_tempered) {
       // Calculate log(sum(exp(V(s,t)/kbT)))
-      const cvm::real const max_exponent = time_independent_grid->maximum_value();
+      const cvm::real max_exponent = time_independent_grid->maximum_value();
       time_independent_grid->add_constant(-1.0 * max_exponent);
-      time_independent_grid->apply(std::exp);
+      time_independent_grid->apply([](cvm::real x){return std::exp(x);});
       cvm::real logsumexp = max_exponent + std::log(time_independent_grid->sum());
       // Calculate Δs
       cvm::real delta_s = 1.0;
@@ -1705,8 +1705,8 @@ void colvarbias_meta::write_pmf()
       // F(s) = -V(s,t) + kbt * c(t)
       pmf->add_constant(-1.0 * kbt * c_t);
     }
-    cvm::real const max = pmf->maximum_value();
-    pmf->add_constant(-1.0 * max);
+//     cvm::real const max = pmf->maximum_value();
+//     pmf->add_constant(-1.0 * max);
     pmf->multiply_constant(-1.0);
     if (well_tempered) {
       cvm::real const well_temper_scale = (bias_temperature + cvm::temperature()) / bias_temperature;
